4d573b078e96b84b574a8d15db23a8fd46e44992
swsk33 <yanhanhui2001a@163.com>
2020-02-13 16:46:02 +0800

    v1.0

--- src/swsk33/Reg/RegAdd.java
+++ src/swsk33/Reg/RegAdd.java
@@ -104,4 +104,30 @@ public class RegAdd {
 			System.out.println("数据类型错误！必须是REG_SZ（字符串值）,REG_MULTI_SZ（多字符串值）,REG_EXPAND_SZ（可扩展字符串值）,REG_DWORD（DWORD值）,REG_QWORD（QWORD值）,REG_BINARY（二进制值）或者REG_NONE（无）(可不分大小写)");
 		}
 	}
+	public void addHKCC(String name) throws Exception {		//在HKEY_CURRENT_CONFIG里面添加空项
+		String cmd="cmd /c reg add HKCC\\"+name+" /f";
+		Process run=Runtime.getRuntime().exec(cmd);
+	}
+	public void addHKCC(String name,String data) throws Exception {		//在HKEY_CURRENT_CONFIG里面添加项并指定其默认项的值，字符串值类型
+		String cmd="cmd /c reg add HKCC\\"+name+" /d "+data+" /f";
+		Process run=Runtime.getRuntime().exec(cmd);
+	}
+	public void addHKCC(String name,String type,String data) throws Exception {		//在HKEY_CURRENT_CONFIG里面添加项并指定其子项类型和值
+		if(type.equals("REG_SZ")||type.equals("REG_MULTI_SZ")||type.equals("REG_EXPAND_SZ")||type.equals("REG_DWORD")||type.equals("REG_QWORD")||type.equals("REG_BINARY")||type.equals("REG_NONE")||type.equals("reg_sz")||type.equals("reg_multi_sz")||type.equals("reg_expand_sz")||type.equals("reg_dword")||type.equals("reg_qword")||type.equals("reg_binary")||type.equals("reg_none")) {
+			String cmd="cmd /c reg add HKCC\\"+name+" /t "+type+" /d "+data+" /f";
+			Process run=Runtime.getRuntime().exec(cmd);
+		} else {
+			Process tip=Runtime.getRuntime().exec("cmd /c echo msgbox \"数据类型错误！必须是REG_SZ（字符串值）,REG_MULTI_SZ（多字符串值）,REG_EXPAND_SZ（可扩展字符串值）,REG_DWORD（DWORD值）,REG_QWORD（QWORD值）,REG_BINARY（二进制值）或者REG_NONE（无）(可不分大小写)\",64,\"ERROR\">alert.vbs && start alert.vbs && ping -n 2 127.1>nul && del alert.vbs");
+			System.out.println("数据类型错误！必须是REG_SZ（字符串值）,REG_MULTI_SZ（多字符串值）,REG_EXPAND_SZ（可扩展字符串值）,REG_DWORD（DWORD值）,REG_QWORD（QWORD值）,REG_BINARY（二进制值）或者REG_NONE（无）(可不分大小写)");
+		}
+	}
+	public void addHKCC(String name,String type,String objname,String data) throws Exception {		//在HKEY_CURRENT_CONFIG里面添加项并指定其子项类型、名称和值
+		if(type.equals("REG_SZ")||type.equals("REG_MULTI_SZ")||type.equals("REG_EXPAND_SZ")||type.equals("REG_DWORD")||type.equals("REG_QWORD")||type.equals("REG_BINARY")||type.equals("REG_NONE")||type.equals("reg_sz")||type.equals("reg_multi_sz")||type.equals("reg_expand_sz")||type.equals("reg_dword")||type.equals("reg_qword")||type.equals("reg_binary")||type.equals("reg_none")) {
+			String cmd="cmd /c reg add HKCC\\"+name+" /t "+type+" /v "+objname+" /d "+data+" /f";
+			Process run=Runtime.getRuntime().exec(cmd);
+		} else {
+			Process tip=Runtime.getRuntime().exec("cmd /c echo msgbox \"数据类型错误！必须是REG_SZ（字符串值）,REG_MULTI_SZ（多字符串值）,REG_EXPAND_SZ（可扩展字符串值）,REG_DWORD（DWORD值）,REG_QWORD（QWORD值）,REG_BINARY（二进制值）或者REG_NONE（无）(可不分大小写)\",64,\"ERROR\">alert.vbs && start alert.vbs && ping -n 2 127.1>nul && del alert.vbs");
+			System.out.println("数据类型错误！必须是REG_SZ（字符串值）,REG_MULTI_SZ（多字符串值）,REG_EXPAND_SZ（可扩展字符串值）,REG_DWORD（DWORD值）,REG_QWORD（QWORD值）,REG_BINARY（二进制值）或者REG_NONE（无）(可不分大小写)");
+		}
+	}
 }

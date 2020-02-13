3e6f7a27409a42fbc4d50540a5c889ea4dab8737
swsk33 <yanhanhui2001a@163.com>
2020-02-13 17:09:14 +0800

    v1.0

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 import swsk33.Reg.*;
 public class Test {
 	public static void main(String[] args) throws Exception {
-		RegAdd ra=new RegAdd();
-		ra.addHKCC("sb");
+		RegDelete rd=new RegDelete();
+		rd.delHKCC("sb");
 	}
 }

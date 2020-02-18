a64ad590b15b71eac4d9a14054f632e704b6e90d
swsk33 <yanhanhui2001a@163.com>
2020-02-18 21:36:58 +0800

    v1.1

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import swsk33.Reg.*;
+public class Test {
+	public static void main(String[] args) throws Exception {
+		RegAdd ra=new RegAdd();
+		RegDelete rd=new RegDelete();
+		RegExport rep=new RegExport();
+		RegImport ri=new RegImport();
+		RegQuery rq=new RegQuery();
+		
+	}
+
+}

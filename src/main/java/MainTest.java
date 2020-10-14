3c1b53473e673288c2de31aa258b030a527e9d67
swsk33 <yanhanhui2001a@163.com>
2020-10-14 20:48:05 +0800

    v2.0.0

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -4,9 +4,7 @@ import swsk33.reg.exception.*;
 public class MainTest {
 
 	public static void main(String[] args) throws Exception {
-		// new RegAdd().addHKCR("*\\shell\\")
-		new RegAdd().addHKCR("*\\shell\\aas", RegAdd.REG_NONE, "aaasa");
-		System.out.println("a");
+		System.out.println(new RegQuery().queryHKLM("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", "Path"));
 	}
 
 }
--- src/main/java/swsk33/reg/exception/TypeErrorException.java
+++ src/main/java/swsk33/reg/exception/TypeErrorException.java
@@ -1,7 +1,16 @@
 package swsk33.reg.exception;
 
 public class TypeErrorException extends Exception {
+
+	/**
+	 * 生成版本序列号
+	 */
+	private static final long serialVersionUID = -1396944163316796410L;
+
 	public TypeErrorException() {
+
 		super("数据类型错误！必须是REG_SZ（字符串值）,REG_MULTI_SZ（多字符串值）,REG_EXPAND_SZ（可扩展字符串值）,REG_DWORD（DWORD值）,REG_QWORD（QWORD值）,REG_BINARY（二进制值）或者REG_NONE（无）(可不分大小写)");
+
 	}
+
 }

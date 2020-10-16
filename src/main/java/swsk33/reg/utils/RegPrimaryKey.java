fb092be8609173b56b3ca06bef5b3765acff6661
swsk33 <yanhanhui2001a@163.com>
2020-10-16 23:02:33 +0800

    v3.0.0

---
+++ src/main/java/swsk33/reg/utils/RegPrimaryKey.java
@@ -0,0 +1,36 @@
+package swsk33.reg.utils;
+
+/**
+ * 注册表主键
+ * 
+ * @author swsk33
+ *
+ */
+public class RegPrimaryKey {
+
+	/**
+	 * 主键：HKEY_CLASSES_ROOT
+	 */
+	public static final String HKCR = "HKCR\\";
+
+	/**
+	 * 主键：HKEY_CURRENT_USER
+	 */
+	public static final String HKCU = "HKCU\\";
+
+	/**
+	 * 主键：HKEY_LOCAL_MACHINE
+	 */
+	public static final String HKLM = "HKLM\\";
+
+	/**
+	 * 主键：HKEY_USERS
+	 */
+	public static final String HKU = "HKU\\";
+
+	/**
+	 * 主键：HKEY_CURRENT_CONFIG
+	 */
+	public static final String HKCC = "HKCC\\";
+
+}

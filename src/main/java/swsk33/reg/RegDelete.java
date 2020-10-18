1f3edb356aa4dc400cb2c7c7593adf350f7d5e60
swsk33 <yanhanhui2001a@163.com>
2020-10-18 10:41:54 +0800

    v3.0.0

--- src/main/java/swsk33/reg/RegDelete.java
+++ src/main/java/swsk33/reg/RegDelete.java
@@ -8,179 +8,41 @@ package swsk33.reg;
  */
 public class RegDelete {
 
-	// 操作HKEY_CLASSES_ROOT
 	/**
-	 * 删除HKEY_CLASSES_ROOT里面的某项及其所有子项
+	 * 删除注册表里面的某项及其所有子项
 	 * 
-	 * @param name 要删除的项的名称
+	 * @param primaryKey 要操作的主键名
+	 * @param name       要删除的项的名称
 	 * @throws Exception 权限不足时抛出异常
 	 */
-	public void delHKCR(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCR\\" + name + "\"" + " /f";
+	public void del(String primaryKey, String name) throws Exception {
+		String cmd = "cmd /c reg delete \"" + primaryKey + name + "\"" + " /f";
 		Process run = Runtime.getRuntime().exec(cmd);
 	}
 
 	/**
-	 * 删除HKEY_CLASSES_ROOT里面的某项的某个值
+	 * 删除注册表里面的某项的某个值
 	 * 
+	 * @param primaryKey 要操作的主键名
 	 * @param name       项的名称
 	 * @param objectName 要删除的值的名称
 	 * @throws Exception 权限不足时抛出异常
 	 */
-	public void delHKCR(String name, String objectName) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCR\\" + name + "\"" + " /v " + "\"" + objectName + "\"" + " /f";
+	public void del(String primaryKey, String name, String objectName) throws Exception {
+		String cmd = "cmd /c reg delete \"" + primaryKey + name + "\"" + " /v " + "\"" + objectName + "\"" + " /f";
 		Process run = Runtime.getRuntime().exec(cmd);
 	}
 
 	/**
-	 * 删除HKEY_CLASSES_ROOT里面的某项的默认值
+	 * 删除（清空）注册表里面的某项的默认值
 	 * 
-	 * @param name 项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKCRve(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCR\\" + name + "\"" + " /ve " + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	// 操作HKEY_CURRENT_USER
-	/**
-	 * 删除HKEY_CURRENT_USER里面的某项及其所有子项
-	 * 
-	 * @param name 要删除的项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKCU(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCU\\" + name + "\"" + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 删除HKEY_CURRENT_USER里面的某项的某个值
-	 * 
+	 * @param primaryKey 要操作的主键名
 	 * @param name       项的名称
-	 * @param objectName 要删除的值的名称
 	 * @throws Exception 权限不足时抛出异常
 	 */
-	public void delHKCU(String name, String objectName) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCU\\" + name + "\"" + " /v " + "\"" + objectName + "\"" + " /f";
+	public void delDefaultValue(String primaryKey, String name) throws Exception {
+		String cmd = "cmd /c reg delete \"" + primaryKey + name + "\"" + " /ve " + " /f";
 		Process run = Runtime.getRuntime().exec(cmd);
 	}
 
-	/**
-	 * 删除HKEY_CURRENT_USER里面的某项的默认值
-	 * 
-	 * @param name 项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKCUve(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCU\\" + name + "\"" + " /ve " + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	// 操作HKEY_LOCAL_MACHINE
-	/**
-	 * 删除HKEY_LOCAL_MACHINE里面的某项及其所有子项
-	 * 
-	 * @param name 要删除的项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKLM(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKLM\\" + name + "\"" + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 删除HKEY_LOCAL_MACHINE里面的某项的某个值
-	 * 
-	 * @param name       项的名称
-	 * @param objectName 要删除的值的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKLM(String name, String objectName) throws Exception {
-		String cmd = "cmd /c reg delete \"HKLM\\" + name + "\"" + " /v " + "\"" + objectName + "\"" + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 删除HKEY_LOCAL_MACHINE里面的某项的默认值
-	 * 
-	 * @param name 项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKLMve(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKLM\\" + name + "\"" + " /ve " + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	// 操作HKEY_USERS
-	/**
-	 * 删除HKEY_USERS里面的某项及其所有子项
-	 * 
-	 * @param name 要删除的项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKU(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKU\\" + name + "\"" + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 删除HKEY_USERS里面的某项的某个值
-	 * 
-	 * @param name       项的名称
-	 * @param objectName 要删除的值的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKU(String name, String objectName) throws Exception {
-		String cmd = "cmd /c reg delete \"HKU\\" + name + "\"" + " /v " + "\"" + objectName + "\"" + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 删除HKEY_USERS里面的某项的默认值
-	 * 
-	 * @param name 项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKUve(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKU\\" + name + "\"" + " /ve " + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	// 操作HKEY_CURRENT_CONFIG
-	/**
-	 * 删除HKEY_CURRENT_CONFIG里面的某项及其所有子项
-	 * 
-	 * @param name 要删除的项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKCC(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCC\\" + name + "\"" + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 删除HKEY_CURRENT_CONFIG里面的某项的某个值
-	 * 
-	 * @param name       项的名称
-	 * @param objectName 要删除的值的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKCC(String name, String objectName) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCC\\" + name + "\"" + " /v " + "\"" + objectName + "\"" + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 删除HKEY_CURRENT_CONFIG里面的某项的默认值
-	 * 
-	 * @param name 项的名称
-	 * @throws Exception 权限不足时抛出异常
-	 */
-	public void delHKCCve(String name) throws Exception {
-		String cmd = "cmd /c reg delete \"HKCC\\" + name + "\"" + " /ve " + " /f";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-	
 }
--- src/main/java/swsk33/reg/RegExport.java
+++ src/main/java/swsk33/reg/RegExport.java
@@ -9,62 +9,15 @@ package swsk33.reg;
 public class RegExport {
 
 	/**
-	 * 导出HKEY_CLASSES_ROOT里面的某一项
+	 * 导出注册表里面的某一项
 	 * 
-	 * @param name     待导出项名称
-	 * @param filePath 导出路径
+	 * @param primaryKey 要操作的主键名
+	 * @param name       待导出项名称
+	 * @param filePath   导出路径
 	 * @throws Exception 权限不足抛出异常
 	 */
-	public void exportHKCR(String name, String filePath) throws Exception {
-		String cmd = "cmd /c reg export \"HKCR\\" + name + "\"" + " " + "\"" + filePath + "\"" + " /y";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 导出HKEY_CURRENT_USER里面的某一项
-	 * 
-	 * @param name     待导出项名称
-	 * @param filePath 导出路径
-	 * @throws Exception 权限不足抛出异常
-	 */
-	public void exportHKCU(String name, String filePath) throws Exception {
-		String cmd = "cmd /c reg export \"HKCU\\" + name + "\"" + " " + "\"" + filePath + "\"" + " /y";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 导出HKEY_LOCAL_MACHINE里面的某一项
-	 * 
-	 * @param name     待导出项名称
-	 * @param filePath 导出路径
-	 * @throws Exception 权限不足抛出异常
-	 */
-	public void exportHKLM(String name, String filePath) throws Exception {
-		String cmd = "cmd /c reg export \"HKLM\\" + name + "\"" + " " + "\"" + filePath + "\"" + " /y";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 导出HKEY_USERS里面的某一项
-	 * 
-	 * @param name     待导出项名称
-	 * @param filePath 导出路径
-	 * @throws Exception 权限不足抛出异常
-	 */
-	public void exportHKU(String name, String filePath) throws Exception {
-		String cmd = "cmd /c reg export \"HKU\\" + name + "\"" + " " + "\"" + filePath + "\"" + " /y";
-		Process run = Runtime.getRuntime().exec(cmd);
-	}
-
-	/**
-	 * 导出HKEY_CURRENT_CONFIG里面的某一项
-	 * 
-	 * @param name     待导出项名称
-	 * @param filePath 导出路径
-	 * @throws Exception 权限不足抛出异常
-	 */
-	public void exportHKCC(String name, String filePath) throws Exception {
-		String cmd = "cmd /c reg export \"HKCC\\" + name + "\"" + " " + "\"" + filePath + "\"" + " /y";
+	public void export(String primaryKey, String name, String filePath) throws Exception {
+		String cmd = "cmd /c reg export \"" + primaryKey + name + "\"" + " " + "\"" + filePath + "\"" + " /y";
 		Process run = Runtime.getRuntime().exec(cmd);
 	}
 
